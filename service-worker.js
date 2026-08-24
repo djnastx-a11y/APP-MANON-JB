fe563423f1971f8836e1f4b35799362f89b303ed
djnastx-a11y <djnastx@gmail.com>
2026-08-24 22:07:58 +0200

    Cache final version 5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="nous-deux-v4";
+const CACHE="nous-deux-v5";
 const ASSETS=["./","./index.html","./app.css","./app.js","./manifest.json","./config.js"];
 
 self.addEventListener("install",event=>{
