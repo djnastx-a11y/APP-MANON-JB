2a50e7ec17591d6ecef3633a2f0d6452f3133a01
djnastx-a11y <djnastx@gmail.com>
2026-08-25 17:14:17 +0200

    Actualiser le cache pour les tâches ouvrables

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="nous-deux-v13";
-const ASSETS=["./","./index.html","./app.css?v=13","./app.js?v=13","./manifest.json","./icon.svg","./config.js"];
+const CACHE="nous-deux-v14";
+const ASSETS=["./","./index.html","./app.css?v=14","./app.js?v=14","./manifest.json","./icon.svg","./config.js"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
