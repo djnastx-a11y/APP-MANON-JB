e4ac145483af44c1997b612bb7e2cadb141ac490
djnastx-a11y <djnastx@gmail.com>
2026-08-25 17:09:39 +0200

    Actualiser le cache de l'application

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="nous-deux-v12";
-const ASSETS=["./","./index.html","./app.css?v=12","./app.js?v=12","./manifest.json","./icon.svg","./config.js"];
+const CACHE="nous-deux-v13";
+const ASSETS=["./","./index.html","./app.css?v=13","./app.js?v=13","./manifest.json","./icon.svg","./config.js"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
