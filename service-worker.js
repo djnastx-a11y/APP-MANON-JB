3fc3da49e101d20a598b6c5ed656128fac8635d6
djnastx-a11y <djnastx@gmail.com>
2026-09-14 12:41:45 +0200

    fix: refresh PWA cache for Life360 core

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="nous-deux-v16";
-const ASSETS=["./","./index.html","./app.css?v=14","./app.js?v=14","./manifest.json","./icon.svg","./config.js","./location.html","./location.css?v=6","./location.js?v=6"];
+const CACHE="nous-deux-v17";
+const ASSETS=["./","./index.html","./app.css?v=14","./app.js?v=14","./manifest.json","./icon.svg","./config.js","./location.html","./location.css?v=17","./location.js?v=17"];
 
 self.addEventListener("install",event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
